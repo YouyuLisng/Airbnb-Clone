c0cfa8bc79bd053b847f8b103749c66ab1ac04c5
YouyuLisng <a0979534311@gmail.com>
2023-11-17 16:44:39 +0800

    202311171644

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import ClientOnly from './components/ClientOnly'
 import Container from './components/Container'
 import EmptyState from './components/EmptyState';
 import ListingCard from './components/Listings/ListingCard';
+import { Listing } from '@prisma/client';
 
 export const dynamic = 'force-dynamic' // 部署會遇到 Error: Dynamic server usage: searchParams.userId
 
@@ -28,7 +29,7 @@ const Home = async ({ searchParams } : HomeProps) => {
         <ClientOnly>
             <Container>
                 <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-                    {listings.map((listing) => {
+                    {listings.map((listing: any) => {
                         return (
                             <ListingCard 
                                 currentUser={currentUser}
