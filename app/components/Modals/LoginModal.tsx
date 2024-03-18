bf8c96bb9b7e542f11362cc04c93201e3df7d93a
YouyuLisng <a0979534311@gmail.com>
2024-03-18 17:50:15 +0800

    202403181750

--- app/components/Modals/LoginModal.tsx
+++ app/components/Modals/LoginModal.tsx
@@ -1,7 +1,6 @@
 'use client';
 "登入Modal"
 import { signIn } from "next-auth/react";
-import axios from "axios";
 import { AiFillGithub, AiFillApple } from "react-icons/ai";
 // import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
