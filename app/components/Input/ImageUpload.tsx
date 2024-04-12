075e49a915d8f8a3c14f6d49ce23c919c3ad626c
YouyuLisng <a0979534311@gmail.com>
2024-04-12 15:22:58 +0800

    202404121522

--- app/components/Input/ImageUpload.tsx
+++ app/components/Input/ImageUpload.tsx
@@ -1,5 +1,4 @@
 "use client"; // 上傳圖片
-
 import { CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
 import { useCallback } from "react";
@@ -24,37 +23,39 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
     }, [onChange]);
 
     return (
-        <CldUploadWidget 
-        onUpload={handleUpload} 
-        uploadPreset="jf3yjihr"
-        options={{
-            maxFiles: 1
-        }}
-        >
-        {({ open }) => {
-            return(
-                <div
-                    onClick={() => open?.()}
-                    className="relative cursor-pointer hover:opacity-70 transition border-dashed border-2 p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600"
-                >
-                    <TbPhotoPlus size={50} />
-                    <div className="font-semibold text-lg">
-                        點擊上傳圖片
-                    </div>
-                    {value &&(
-                        <div className=" absolute inset-0 w-full h-full">
-                            <Image 
-                                alt="Upload"
-                                fill
-                                style={{ objectFit: 'cover' }}
-                                src={value}
-                            />
-                        </div>
-                    )}
-                </div>
-            )
-        }}
-        </CldUploadWidget>
+        // <CldUploadWidget 
+        //     onUpload={handleUpload} 
+        //     uploadPreset="jf3yjihr"
+        //     options={{
+        //         maxFiles: 1
+        //     }}
+        // >
+        // {({ open }) => {
+        //     return(
+        //         <div
+        //             onClick={() => open?.()}
+        //             className="relative cursor-pointer hover:opacity-70 transition border-dashed border-2 p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600"
+        //         >
+        //             <TbPhotoPlus size={50} />
+        //             <div className="font-semibold text-lg">
+        //                 點擊上傳圖片
+        //             </div>
+        //             {value &&(
+        //                 <div className=" absolute inset-0 w-full h-full">
+        //                     <Image 
+        //                         alt="Upload"
+        //                         fill
+        //                         style={{ objectFit: 'cover' }}
+        //                         src={value}
+        //                     />
+        //                 </div>
+        //             )}
+        //         </div>
+        //     )
+        // }}
+        // </CldUploadWidget>
+        <>
+        </>
     );
 }
 
